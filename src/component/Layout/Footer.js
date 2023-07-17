d100d5e0cc57341896ba055c89765eb62bee5ab8
Rafho <rafhoerdeft@gmail.com>
2023-07-17 11:01:32 +0700

    Layouting component

---
+++ src/component/Layout/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>&copy; ErdevApp</strong> by <a href="https://erdevapp.com">Rafho Erdeft</a>.
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
